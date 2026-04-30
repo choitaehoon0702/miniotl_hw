1bee0690bdc37167dbca3694f37b8f68187a8ece
autumn <autumn@sparcs.org>
2026-04-30 15:38:33 +0900

    Todo27: NestJS bootstrap

--- src/main.ts
+++ src/main.ts
@@ -22,9 +22,24 @@ import { requestLogMiddlware } from './requestLog.middleware';
 // ===========================================================================
 
 async function bootstrap() {
-  // TODO: NestJS 앱을 생성하고 미들웨어/파이프를 설정하세요.
   const app = await NestFactory.create(AppModule);
-  // TODO: cookieParser, ValidationPipe, 조건부 requestLogMiddlware를 등록하세요.
+
+  app.use(cookieParser());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
+  const configService = app.get(ConfigService);
+  const logRequests = configService.get('LOG_REQUESTS');
+
+  if (logRequests === 'true') {
+    app.use(requestLogMiddlware);
+  }
+
   await app.listen(3000);
 }
+
 bootstrap();
